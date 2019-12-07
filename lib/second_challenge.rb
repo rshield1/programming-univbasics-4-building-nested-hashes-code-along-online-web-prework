8d17674abc75b6f2f6626d3c4ae8b5806aef3432
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-07 20:47:37 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,11 +5,17 @@ def second_challenge
       :patriarch => {}
       :matriarch => {}
       :hero => {}
-      :hero_friends = []
+      :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
-      }}
+      :patriarch => {}
+      :matriarch => {}
+      :heroine => {}
+      :heroine_friends => []
+      }
+    
+  }
 
   
 end
